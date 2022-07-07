df7295f8aa3b6c2fff5072239cc2088266983587
Nguyen Tinh <vantinhnguyen728@gmail.com>
2022-07-07 15:52:16 +0700

    config router page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { BrowserRouter } from 'react-router-dom'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  )
 }
 
 export default MyApp
