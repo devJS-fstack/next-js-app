4b95a0dbb2e2c28ac6474ea970eb8c9a1cb3d1f6
Nguyen Tinh <vantinhnguyen728@gmail.com>
2022-07-07 14:44:29 +0700

    use context with modal detail services

--- components/services.tsx
+++ components/services.tsx
@@ -1,19 +1,5 @@
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
+import { createContext, useState } from 'react'
+import DetailComponent from './detailservice'
 const products = [
     {
         id: 1,
@@ -55,38 +41,47 @@ const products = [
 
 ]
 
-export default function ServiceComponent() {
-    return (
-        <div className="bg-white">
-            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
+export const ModalDetailContext: any = createContext([])
 
-                <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {products.map((product) => (
-                        <div key={product.id} className="group relative">
-                            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                                <img
-                                    src={product.imageSrc}
-                                    alt={product.imageAlt}
-                                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                                />
-                            </div>
-                            <div className="mt-4 flex justify-between">
-                                <div>
-                                    <h3 className="text-sm text-gray-700">
-                                        <a href={product.href}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
-                                            {product.name}
-                                        </a>
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+export default function ServiceComponent() {
+    const [show, setShow] = useState(false)
+    const handleShow = () => {
+        setShow(!show)
+    }
+    return (
+        <ModalDetailContext.Provider value={[show, setShow]}>
+            <div className="bg-white">
+                <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
+
+                    <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                        {products.map((product) => (
+                            <div key={product.id} className="group relative" onClick={handleShow}>
+                                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                                    <img
+                                        src={product.imageSrc}
+                                        alt={product.imageAlt}
+                                        className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                                    />
                                 </div>
-                                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                                <div className="mt-4 flex justify-between">
+                                    <div>
+                                        <h3 className="text-sm text-gray-700">
+                                            <a href={product.href} onClick={(e) => { e.preventDefault() }}>
+                                                <span aria-hidden="true" className="absolute inset-0" />
+                                                {product.name}
+                                            </a>
+                                        </h3>
+                                        <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                    </div>
+                                    <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+            <DetailComponent></DetailComponent>
+        </ModalDetailContext.Provider>
     )
 }
