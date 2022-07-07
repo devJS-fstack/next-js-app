d793c6c875073fe6b86a0faf330654ec232d4237
Nguyen Tinh <vantinhnguyen728@gmail.com>
2022-07-07 21:42:24 +0700

    fix bug active navbar on mobile

--- components/header.tsx
+++ components/header.tsx
@@ -154,19 +154,22 @@ export default function Header() {
                     </div>
                     <Disclosure.Panel className="sm:hidden">
                         <div className="px-2 pt-2 pb-3 space-y-1">
-                            {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                        'block px-3 py-2 rounded-md text-base font-medium'
-                                    )}
-                                    aria-current={item.current ? 'page' : undefined}
-                                >
-                                    {item.name}
-                                </Disclosure.Button>
+                            {navigation.map((item, index) => (
+                                <Link href={item.href} key={item.id}>
+                                    <Disclosure.Button
+                                        onClick={() => { handleSwitchPage(index) }}
+                                        key={item.name}
+                                        as="a"
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'block px-3 py-2 rounded-md text-base font-medium'
+                                        )}
+                                        aria-current={item.current ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </Disclosure.Button>
+                                </Link>
                             ))}
                         </div>
                     </Disclosure.Panel>
