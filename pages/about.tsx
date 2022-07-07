a89c43a10d962ccb73559854f825aea617c27167
Nguyen Tinh <vantinhnguyen728@gmail.com>
2022-07-07 17:27:52 +0700

    config 2 page --about & --signup

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import ProfileComponent from "../components/profile";
+import Header from "../components/header";
+
+export default function AboutMe() {
+    return (
+        <>
+            <Header></Header>
+            <ProfileComponent></ProfileComponent>
+        </>
+    )
+}
---
+++ pages/signup.tsx
@@ -0,0 +1,10 @@
+import FormComponent from "../components/form";
+import Header from '../components/header'
+export default function SignUp() {
+    return (
+        <>
+            <Header></Header>
+            <FormComponent></FormComponent>
+        </>
+    )
+}
