99218a8e760dd5aec677abe93477efe629116243
Nguyen Tinh <vantinhnguyen728@gmail.com>
2022-07-11 21:20:36 +0700

    done box vertical & responsive

---
+++ components/home/box/box_vertical.tsx
@@ -0,0 +1,9 @@
+import Vertical from '../effects/vertical'
+
+export default function BoxVertical() {
+    return (
+        <div className="box box-work-1 box-2h">
+            <Vertical></Vertical>
+        </div>
+    )
+}
--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import BoxIntroduce from './box/box_introduce'
 import BoxMap from './box/box_map'
+import BoxVertical from './box/box_vertical'
 export default function Home() {
     return (
         <>
@@ -9,6 +10,7 @@ export default function Home() {
                     <div className="main_grid">
                         <BoxIntroduce></BoxIntroduce>
                         <BoxMap></BoxMap>
+                        <BoxVertical></BoxVertical>
                     </div>
                 </div>
             </main>
